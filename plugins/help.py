f945688a2ccf2a5c1798e475cfa8069322ae6994
medeor413 <medeor413@users.noreply.github.com>
2017-07-31 13:33:53 -0400

    Better capitalization in help

--- plugins/help.py
+++ plugins/help.py
@@ -1,4 +1,5 @@
 import asyncio
+from string import capwords
 from plugin_manager import BasePlugin
 from utils import respond, Command
 
@@ -30,25 +31,25 @@ class Help(BasePlugin):
         try:
             search = data.clean_content.split(" ")[1].lower()
         except IndexError:
-            cates = "\n".join([x.capitalize() for x in self.categories.keys()])
+            cates = "\n".join([capwords(x, "_") for x in self.categories.keys()])
             await respond(self.client, data, f"**ANALYSIS: Command categories:**```\n{cates}\n```")
             return
         if search in [x.lower() for x in self.commands.keys()]:
             cmd = self.commands[search]
-            name = search.capitalize()
+            name = capwords(search, "_")
             syn = cmd.syntax
             if not syn:
                 syn = "N/A"
             doc = cmd.__doc__
             perms = cmd.perms
-            cate = cmd.category.capitalize()
+            cate = capwords(cmd.category, "_")
             if not {x for x, y in data.author.server_permissions if y} >= perms:
                 raise PermissionError
             text = f"**ANALYSIS: Command {name}:**```\n{name} (Category {cate})\n{doc}\nSyntax: {syn}\n```"
             await respond(self.client, data, text)
         elif search in self.categories.keys():
-            name = search.capitalize()
-            text = "\n".join([x["name"].capitalize() for x in self.categories[search].values()])
+            name = capwords(search, "_")
+            text = "\n".join([capwords(x["name"], "_") for x in self.categories[search].values()])
             await respond(self.client, data, f"**ANALYSIS: Category {name}:**```\n{text}\n```")
         else:
             await respond(self.client, data, f"**WARNING: No such category or command {search}**")
