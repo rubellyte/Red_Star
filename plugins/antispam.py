46fb83bd99a75f0fe66206c86db4b9815bf857ad
MekaTriK <gtg3013@gmail.com>
2017-08-12 03:46:20 +0300

    Implemented the infraction system in antispam.py Infractions accumulate for spamming, triggering escalating response. * Reaction to message * Deletion of message * Application of a role for a set parameter (presumably to mute) * Ban Config includes customisations of: * Toggling each individual response on and off. * Time and amount of messages during that time considered "not spam" * Infraction levels for each response (additive) * Infraction timeout period. Currently available commands: * spam_emoji : customises or turns off the reaction response * spam_role : customises or turns off the role response

--- plugins/antispam.py
+++ plugins/antispam.py
@@ -1,8 +1,13 @@
 from plugin_manager import BasePlugin
 from utils import Command, respond
-from discord import HTTPException
-import time
+from discord import HTTPException, Forbidden
+from discord.object import Object as DObj
+from math import ceil
 import re
+import time
+import threading
+import asyncio
+
 
 class AntiSpam(BasePlugin):
     name = "anti_spam"
@@ -17,21 +22,125 @@ class AntiSpam(BasePlugin):
             'spam_reaction': False,      # Add a reaction to messages treated as spam. ID or False #232885229352779776
             'thresholds': [1, 1, 2, 2],  # react - delet - mute - ban
             'infraction_timeout': 300    # get another infraction if you're stupid enough to spam repeatedly
-        }
+        },
+        'poll_every': 5
     }
+
+    class UserStorage:
+        member = False
+        guild = False
+        parent = False
+        infractions = 0
+        update_time = 0
+        messages = 0
+        needs_reinit = False
+        muted = False
+
+        def __init__(self, member, guild, parent):
+            """
+
+            :type parent: AntiSpam
+            """
+            self.member = member
+            self.guild = guild
+            self.parent = parent
+            self.infractions = 0
+            self.update_time = time.time()
+
+        def __getstate__(self):
+            result = {
+                'member': self.member.id,
+                'guild': self.guild.id,
+                'infractions': self.infractions,
+                'update_time': self.update_time,
+                'messages': self.messages,
+                'muted': self.muted
+            }
+            return result
+
+        def __setstate__(self, state):
+            self.member = state["member"]
+            self.guild = state["guild"]
+            self.infractions = state["infractions"]
+            self.update_time = state["update_time"]
+            self.messages = state["messages"]
+            self.muted = state.get("muted", False)
+            self.needs_reinit = True
+
+        def reinit(self, parent):
+            self.parent = parent
+            self.guild = parent.client.get_guild(self.guild)
+            self.member = self.guild.get_member(self.member)
+            self.needs_reinit = False
+
+        async def infract(self):
+            if self.needs_reinit:
+                return
+            if self.guild.id not in self.parent.s_thresholds:
+                return
+            if len(self.parent.s_thresholds[self.guild.id]) != 4:
+                return
+            t_thold = self.parent.s_thresholds[self.guild.id]
+            self.infractions += 1
+            print(f"Infractions of {self.member.display_name} : {self.infractions}")
+            self.update_time = time.time()
+            t_config = self.parent.plugin_config[str(self.guild.id)]
+            if self.infractions >= t_thold[3] and t_config["spam_ban"]:
+                try:
+                    self.parent.logger.info(f"Banned member {self.member.display_name}")
+                    await self.guild.ban(self.member, reason="Banned for repeated spamming.", delete_message_days=0)
+                except Forbidden:
+                    self.parent.logger.warning(f"Can't edit member {self.member.display_name}")
+            elif self.infractions >= t_thold[2] and t_config["spam_role"]:
+                try:
+                    await self.member.add_roles(DObj(t_config["spam_role"]),
+                                                reason="Muting for spam. (applying a specified role.)")
+                except Forbidden:
+                    self.parent.logger.warning(f"Can't edit member {self.member.display_name}")
+                except HTTPException:
+                    self.parent.logger.warning(f"Member {self.member.display_name} already has mute role.")
+                else:
+                    self.parent.logger.info(f"Muted member {self.member.display_name}")
+                    self.muted = True
+
+        async def update(self):
+            if self.needs_reinit:
+                return
+            t_config = self.parent.plugin_config[str(self.guild.id)]
+            if t_config["spam_role"] and time.time() - self.update_time > t_config['spam_role_timeout'] and self.muted:
+                for t_role in self.member.roles:
+                    if t_role.id == int(t_config["spam_role"]):
+                        try:
+                            await self.member.remove_roles(t_role, reason="Mute time ran out.")
+                        except Forbidden:
+                            self.parent.logger.warning(f"Can't edit member {self.member.display_name}")
+                        else:
+                            self.muted = False
+                        break
+            if time.time() - self.update_time > t_config['infraction_timeout']:
+                self.infractions = max(0, self.infractions-1)
+                self.update_time = time.time()
 
     async def activate(self):
         self.run_timer = True
-        self.members = {}
-        if "muted_members" not in self.storage:
-            self.storage["muted_members"] = {}
+        self.s_thresholds = {}
+        if "members" not in self.storage:
+            self.storage["members"] = {}
         for guild in self.client.guilds:
-            self.members[guild.id] = {}
-            if str(guild.id) not in self.storage["muted_members"]:
-                self.storage["muted_members"][str(guild.id)] = {}
+            if guild.id not in self.storage["members"]:
+                self.storage["members"][guild.id] = {}
+            else:
+                for k, t_member in self.storage["members"][guild.id].items():
+                    t_member.reinit(self)
             if str(guild.id) not in self.plugin_config:
                 self.plugin_config[str(guild.id)] = self.plugin_config["default"]
+            self.calc_thresholds(guild)
 
+        loop = asyncio.new_event_loop()
+        t_loop = asyncio.get_event_loop()
+        self.timer = threading.Thread(target=self.start_timer, args=[loop, t_loop])
+        self.timer.setDaemon(True)
+        self.timer.start()
 
     async def deactivate(self):
         self.run_timer = False
@@ -54,13 +163,19 @@ class AntiSpam(BasePlugin):
         if msg.author == msg.guild.me:
             return
 
-        if msg.author.id in self.members[msg.guild.id]:
-            t_member = self.members[msg.guild.id][msg.author.id]
+        if msg.author.id in self.storage["members"][msg.guild.id]:
+            t_member = self.storage["members"][msg.guild.id][msg.author.id]
             t_config = self.plugin_config[str(msg.guild.id)]
-            if time.time() - t_member[0] < t_config["timeout"]:
-                t_member[1] += 1
-                if t_member[1] > t_config["message_count"]:
-                    if t_config["spam_reaction"]:
+            if time.time() - t_member.update_time < t_config["timeout"]:
+                t_member.messages += 1
+                if t_member.messages >= t_config["message_count"]:
+                    if t_member.messages % t_config["message_count"] == 0:
+                        await t_member.infract()
+                    else:
+                        t_member.update_time = time.time()
+                    if t_member.infractions >= self.s_thresholds[msg.guild.id][1] and t_config["spam_delete"]:
+                        await msg.delete(reason="Spam filtering.")
+                    elif t_member.infractions >= self.s_thresholds[msg.guild.id][0] and t_config["spam_reaction"]:
                         if len(t_config["spam_reaction"]) != 18:
                             try:
                                 await msg.add_reaction(t_config["spam_reaction"])
@@ -68,26 +183,114 @@ class AntiSpam(BasePlugin):
                                 self.logger.error(f"Non-emoji character set as spam reaction on server {msg.guild.id}")
                         else:
                             await msg.add_reaction(self.client.get_emoji(int(t_config["spam_reaction"])))
-                    if t_config["spam_delete"]:
-                        await msg.delete(reason="Spam filtering.")
-                    t_member[0] = time.time()
             else:
-                t_member[1] = 1
-                t_member[0] = time.time()
+                t_member.messages = 1
+                t_member.update_time = time.time()
         else:
-            self.members[msg.guild.id][msg.author.id] = [time.time(), 1]
+            self.storage["members"][msg.guild.id][msg.author.id] = self.UserStorage(msg.author, msg.guild, self)
 
     # Commands
 
-    @Command("getmoji")
+    @Command("spam_emoji",
+             perms={"manage_guild"},
+             doc="Sets the emoji to react to early spam with, or turns that off.",
+             syntax="[emoji]")
     async def _getmoji(self, msg):
         args = msg.content.split()
         if len(args) > 1:
             if len(args[1]) == 1:
-                self.plugin_config[str(msg.guild.id)]["spam_reaction"] = args[1]
+                try:
+                    await msg.add_reaction(args[1])
+                except HTTPException:
+                    raise SyntaxError(f"Non-emoji character set as spam reaction on server.")
+                else:
+                    self.plugin_config[str(msg.guild.id)]["spam_reaction"] = args[1]
+                    await respond(msg, f"**AFFIRMATIVE. ANALYSIS: New spam reaction emoji: {args[1]}.**")
+                    await msg.remove_reaction(args[1], msg.guild.me)
             elif re.fullmatch("<:\w{1,32}:\d{18}>", args[1]):
                 t_emoji = re.search("\d{18}", args[1])[0]
                 if self.client.get_emoji(int(t_emoji)):
                     self.plugin_config[str(msg.guild.id)]["spam_reaction"] = t_emoji
+                    await respond(msg, f"**AFFIRMATIVE. ANALYSIS: New spam reaction emoji: {args[1]}.**")
+            else:
+                raise SyntaxError("Expected a single emoji as argument.")
         else:
-            raise SyntaxError("Expected an emoji as argument.")
+            self.plugin_config[str(msg.guild.id)]["spam_reaction"] = False
+            await respond(msg, f"**AFFIRMATIVE. Spam reaction disabled.**")
+
+    @Command("spam_role")
+    async def _getrole(self, msg):
+        args = msg.content.split()
+        if len(args) > 1:
+            if re.fullmatch("\d{18}", args[1]):
+                for role in msg.guild.roles:
+                    if role.id == int(args[1]):
+                        self.plugin_config[str(msg.guild.id)]["spam_role"] = args[1]
+                        await respond(msg, f"**AFFIRMATIVE. ANALYSIS: New anti-spam role: {role.name}**")
+                        break
+                else:
+                    await respond(msg, "**NEGATIVE. Invalid role ID.**")
+            else:
+                for role in msg.guild.roles:
+                    if role.name.lower() == args[1].lower():
+                        self.plugin_config[str(msg.guild.id)]["spam_role"] = str(role.id)
+                        await respond(msg, f"**AFFIRMATIVE. ANALYSIS: New anti-spam role: {role.name}**")
+                        break
+                else:
+                    await respond(msg, "**NEGATIVE. Invalid role ID.**")
+        else:
+            await respond(msg, "**NEGATIVE. Expected role ID or Name.**")
+
+    @Command("spam_list")
+    async def _spamlist(self, msg):
+        t_string = ""
+        for _, t_member in self.storage["members"][msg.guild.id].items():
+            if t_member.infractions > 0:
+                t_m, t_s = divmod(ceil(time.time()-t_member.update_time), 60)
+                t_string = f"{t_string} {t_member.member.display_name.ljust(32)} : " \
+                           f"{t_member.infractions:02d} [{t_m:02d}:{t_s:02d}]\n"
+        await respond(msg, f"**ANALYSIS: Members in spam list:**\n```{t_string}```")
+
+    # Miscellaneous
+
+    def calc_thresholds(self, guild):
+        if guild.id not in self.s_thresholds:
+            self.s_thresholds[guild.id] = [0, 0, 0, 0]
+        t_lst = self.s_thresholds[guild.id]
+        t_config = self.plugin_config[str(guild.id)]
+        for i in range(4):
+            if i == 0 and t_config["spam_reaction"]:
+                t_lst[i] = t_config["thresholds"][i]
+            if i >= 1:
+                t_lst[i] = t_lst[i-1]
+            if i == 1 and t_config["spam_delete"]:
+                t_lst[i] += t_config["thresholds"][i]
+            if i == 2 and t_config["spam_role"]:
+                t_lst[i] += t_config["thresholds"][i]
+            if i == 3 and t_config["spam_ban"]:
+                t_lst[i] += t_config["thresholds"][i]
+
+    def start_timer(self, loop, t_loop):
+        asyncio.set_event_loop(loop)
+        try:
+            loop.run_until_complete(self.update_regular(t_loop))
+        except Exception:
+            self.logger.exception("Error starting timer. ", exc_info=True)
+
+    async def update_regular(self, loop):
+        while self.run_timer:
+            await asyncio.sleep(self.plugin_config["poll_every"])
+            for k, t_guild in self.storage["members"].items():
+                t_lst = []
+                for k1, t_member in t_guild.items():
+                    if t_member.member and t_member.guild.get_member(t_member.member.id):
+                        t_future = asyncio.run_coroutine_threadsafe(t_member.update(), loop=loop)
+                        try:
+                            t_future.result()
+                        except Exception as e:
+                            print(e)
+                    else:
+                        t_lst.append(k1)
+                for k1 in t_lst:
+                    t_guild.pop(k1)
+
