95c46a56037a2e9818e3589fc032bd060e70870b
MekaTriK <gtg3013@gmail.com>
2017-09-23 02:43:24 +0300

    Role plugin now uses find_role(). Also updated !listrole

--- plugins/roles.py
+++ plugins/roles.py
@@ -1,7 +1,7 @@
 from discord import InvalidArgument, HTTPException, Forbidden, Colour
 from plugin_manager import BasePlugin
 from rs_errors import CommandSyntaxError
-from rs_utils import respond, split_message, is_positive
+from rs_utils import respond, is_positive, find_role, split_output
 from command_dispatcher import Command
 from string import capwords
 import shlex
@@ -13,7 +13,7 @@ class RoleCommands(BasePlugin):
     @Command("EditRole",
              perms={"manage_roles"},
              category="roles",
-             syntax="(role name) [name=string][colour=FFFFFF][hoist=bool][mentionable=bool][position=integer].\n"
+             syntax="(role) [name=string][colour=FFFFFF][hoist=bool][mentionable=bool][position=integer].\n"
                     "ANALYSIS: Strings can be encapsulated in \"...\" to allow spaces",
              doc="Edits the specified role name, colour, hoist (show separately from others)"
                  " and mentionable properties.\n"
@@ -30,41 +30,40 @@ class RoleCommands(BasePlugin):
         try:
             args = shlex.split(msg.content)
         except ValueError as e:
-            self.logger.warning("Unable to split {data.content}. {e}")
+            self.logger.warning(f"Unable to split {msg.content}. {e}")
             raise CommandSyntaxError(e)
         if len(args) > 1:
-            for role in msg.guild.roles:
-                if args[1].lower() == role.name.lower():  # found role
-                    t_dict = {}
-                    for arg in args[2:]:
-                        t_arg = arg.split("=")
-                        if len(t_arg) > 1:  # beautiful
-                            if t_arg[0].lower() == "name":
-                                t_dict["name"] = t_arg[1]
-                            elif t_arg[0].lower() == "colour":
-                                t_dict["colour"] = Colour(int(t_arg[1], 16))
-                            elif t_arg[0].lower() == "color":
-                                t_dict["colour"] = Colour(int(t_arg[1], 16))
-                            elif t_arg[0].lower() == "hoist":
-                                t_dict["hoist"] = is_positive(t_arg[1])
-                            elif t_arg[0].lower() == "mentionable":
-                                t_dict["mentionable"] = is_positive(t_arg[1])
-                            elif t_arg[0].lower() == "position":
-                                if t_arg[1].isdecimal():
-                                    pos = int(t_arg[1])
-                                    if pos >= 0:
-                                        t_dict["position"] = pos
-                                else:
-                                    raise CommandSyntaxError("Position must be a positive integer.")
-                    if len(t_dict) == 0:  # you're wasting my time
-                        raise CommandSyntaxError
-                    await role.edit(**t_dict)
-                    t_string = ""
-                    for k, v in t_dict.items():
-                        t_string = f"{t_string}{k}: {v!s}\n"
-                    name = args[1].capitalize()
-                    await respond(msg, f"**AFFIRMATIVE. Role {name} modified with parameters :**\n ```{t_string}```")
-                    break
+            role = find_role(msg.guild, args[1])
+            if role:
+                t_dict = {}
+                for arg in args[2:]:
+                    t_arg = arg.split("=")
+                    if len(t_arg) > 1:  # beautiful
+                        if t_arg[0].lower() == "name":
+                            t_dict["name"] = t_arg[1]
+                        elif t_arg[0].lower() == "colour":
+                            t_dict["colour"] = Colour(int(t_arg[1], 16))
+                        elif t_arg[0].lower() == "color":
+                            t_dict["colour"] = Colour(int(t_arg[1], 16))
+                        elif t_arg[0].lower() == "hoist":
+                            t_dict["hoist"] = is_positive(t_arg[1])
+                        elif t_arg[0].lower() == "mentionable":
+                            t_dict["mentionable"] = is_positive(t_arg[1])
+                        elif t_arg[0].lower() == "position":
+                            if t_arg[1].isdecimal():
+                                pos = int(t_arg[1])
+                                if pos >= 0:
+                                    t_dict["position"] = pos
+                            else:
+                                raise CommandSyntaxError("Position must be a positive integer.")
+                if len(t_dict) == 0:  # you're wasting my time
+                    raise CommandSyntaxError
+                await role.edit(**t_dict)
+                t_string = ""
+                for k, v in t_dict.items():
+                    t_string = f"{t_string}{k}: {v!s}\n"
+                name = args[1].capitalize()
+                await respond(msg, f"**AFFIRMATIVE. Role {name} modified with parameters :**\n ```{t_string}```")
             else:
                 await respond(msg, f"**NEGATIVE. ANALYSIS: no role {args[1].capitalize()} found.**")
         else:
@@ -85,59 +84,58 @@ class RoleCommands(BasePlugin):
         try:
             args = shlex.split(msg.content)
         except ValueError as e:
-            self.logger.warning("Unable to split {data.content}. {e}")
+            self.logger.warning(f"Unable to split {msg.content}. {e}")
             raise CommandSyntaxError(e)
         if len(args) > 2:
-            for role in msg.guild.roles:
-                if args[2].lower() == role.name.lower():
-                    # copying the existing role (especially permissions)
-                    t_dict = {
-                        "name": args[1],
-                        "permissions": role.permissions,
-                        "colour": role.colour,
-                        "hoist": role.hoist,
-                        "mentionable": role.mentionable
-                    }
-                    rolepos = role.position
-                    for arg in args[3:]:
-                        t_arg = arg.split("=")
-                        if len(t_arg) > 1:  # beautiful
-                            if t_arg[0].lower() == "name":
-                                t_dict["name"] = t_arg[1]
-                            elif t_arg[0].lower() == "colour":
-                                t_dict["colour"] = Colour(int(t_arg[1], 16))
-                            elif t_arg[0].lower() == "color":
-                                t_dict["colour"] = Colour(int(t_arg[1], 16))
-                            elif t_arg[0].lower() == "hoist":
-                                t_dict["hoist"] = is_positive(t_arg[1])
-                            elif t_arg[0].lower() == "mentionable":
-                                t_dict["mentionable"] = is_positive(t_arg[1])
-                            elif t_arg[0].lower() == "position":
-                                if t_arg[1].isdecimal():
-                                    pos = int(t_arg[1])
-                                    if pos > 0:
-                                        rolepos = pos
-                                else:
-                                    raise CommandSyntaxError("Position must be a positive integer.")
-                    t_role = await msg.guild.create_role(**t_dict)
-                    try:
-                        # since I can't create a role with a preset position :T
-                        await t_role.edit(position=rolepos)
-                    except (InvalidArgument, HTTPException):
-                        # oh hey, why are we copying this role again?
-                        name = args[1].capitalize()
-                        await t_role.delete()
-                        await respond(msg, f"**WARNING: Failed to move role {name} to position {rolepos}.**")
-                        raise Forbidden  # yeah, we're not copying this
-                    t_string = ""
-                    for k, v in t_dict.items():
-                        if k != "permissions":
-                            t_string = f"{t_string}{k}: {v!s}\n"
-                        else:
-                            t_string += k + ": " + ", ".join({x.upper() for x, y in v if y}) + "\n"
+            role = find_role(msg.guild, args[2])
+            if role:
+                # copying the existing role (especially permissions)
+                t_dict = {
+                    "name": args[1],
+                    "permissions": role.permissions,
+                    "colour": role.colour,
+                    "hoist": role.hoist,
+                    "mentionable": role.mentionable
+                }
+                rolepos = role.position
+                for arg in args[3:]:
+                    t_arg = arg.split("=")
+                    if len(t_arg) > 1:  # beautiful
+                        if t_arg[0].lower() == "name":
+                            t_dict["name"] = t_arg[1]
+                        elif t_arg[0].lower() == "colour":
+                            t_dict["colour"] = Colour(int(t_arg[1], 16))
+                        elif t_arg[0].lower() == "color":
+                            t_dict["colour"] = Colour(int(t_arg[1], 16))
+                        elif t_arg[0].lower() == "hoist":
+                            t_dict["hoist"] = is_positive(t_arg[1])
+                        elif t_arg[0].lower() == "mentionable":
+                            t_dict["mentionable"] = is_positive(t_arg[1])
+                        elif t_arg[0].lower() == "position":
+                            if t_arg[1].isdecimal():
+                                pos = int(t_arg[1])
+                                if pos > 0:
+                                    rolepos = pos
+                            else:
+                                raise CommandSyntaxError("Position must be a positive integer.")
+                t_role = await msg.guild.create_role(**t_dict)
+                try:
+                    # since I can't create a role with a preset position :T
+                    await t_role.edit(position=rolepos)
+                except (InvalidArgument, HTTPException):
+                    # oh hey, why are we copying this role again?
                     name = args[1].capitalize()
-                    await respond(msg, f"**AFFIRMATIVE. Created role {name} with parameters :**\n ```{t_string}```")
-                    break
+                    await t_role.delete()
+                    await respond(msg, f"**WARNING: Failed to move role {name} to position {rolepos}.**")
+                    raise Forbidden  # yeah, we're not copying this
+                t_string = ""
+                for k, v in t_dict.items():
+                    if k != "permissions":
+                        t_string = f"{t_string}{k}: {v!s}\n"
+                    else:
+                        t_string += k + ": " + ", ".join({x.upper() for x, y in v if y}) + "\n"
+                name = args[1].capitalize()
+                await respond(msg, f"**AFFIRMATIVE. Created role {name} with parameters :**\n ```{t_string}```")
             else:
                 await respond(msg, f"**NEGATIVE. ANALYSIS: no base role {args[2].capitalize()} found.**")
         else:
@@ -146,13 +144,13 @@ class RoleCommands(BasePlugin):
     @Command("DeleteRole",
              perms={"manage_roles"},
              category="roles",
-             syntax="(role name) [position].\nANALYSIS: Strings can be encapsulated in \"...\" to allow spaces",
+             syntax="(role) [position].\nANALYSIS: Strings can be encapsulated in \"...\" to allow spaces",
              doc="Deletes first encounter of the role with the given name and optionally position.")
     async def _deleterole(self, msg):
         try:
             args = shlex.split(msg.content)
         except ValueError as e:
-            self.logger.warning("Unable to split {data.content}. {e}")
+            self.logger.warning(f"Unable to split {msg.content}. {e}")
             raise CommandSyntaxError(e)
         if len(args) > 1:
             name = args[1].capitalize()
@@ -162,13 +160,12 @@ class RoleCommands(BasePlugin):
                     pos = int(args[2])
                 except ValueError:
                     raise SyntaxWarning("Position should be integer.")
-            for role in msg.guild.roles:
+            role = find_role(msg.guild, args[1])
+            if role and (((pos >= 0) and (role.position == pos)) or pos < 0):
                 # delete if name matches, and if pos is not -1 - if position matches
-                if (args[1].lower() == role.name.lower()) and (((pos >= 0) and (role.position == pos)) or pos < 0):
-                    t_position = role.position
-                    await role.delete()
-                    await respond(msg, f"**AFFIRMATIVE. Deleted role: {name} in position: {str(t_position)}.**")
-                    break
+                t_position = role.position
+                await role.delete()
+                await respond(msg, f"**AFFIRMATIVE. Deleted role: {name} in position: {str(t_position)}.**")
             else:
                 await respond(msg, f"**NEGATIVE. ANALYSIS: no role {name} found.**")
         else:
@@ -176,7 +173,7 @@ class RoleCommands(BasePlugin):
 
     @Command("RoleInfo", "InfoRole",
              category="roles",
-             syntax="(role name).\nANALYSIS: Strings can be encapsulated in \"...\" to allow spaces",
+             syntax="(role).\nANALYSIS: Strings can be encapsulated in \"...\" to allow spaces",
              doc="Returns all the info about the given role.")
     async def _inforole(self, msg):
         """
@@ -189,26 +186,25 @@ class RoleCommands(BasePlugin):
             raise CommandSyntaxError(e)
         if len(args) > 1:
             name = capwords(args[1])
-            for role in msg.guild.roles:
-                if args[1].lower() == role.name.lower():
-                    t_dict = {
-                        "name": role.name,
-                        "permissions": role.permissions,
-                        "colour": role.colour,
-                        "hoist": role.hoist,
-                        "mentionable": role.mentionable,
-                        "position": role.position,
-                        "created_at": role.created_at,
-                        "id": role.id
-                    }
-                    t_string = ""
-                    for k, v in t_dict.items():
-                        if k != "permissions":
-                            t_string = f"{t_string}{k}: {v!s}\n"
-                        else:
-                            t_string += k + ": " + ", ".join({x.upper() for x, y in v if y}) + "\n"
-                    await respond(msg, f"**ANALYSIS: role {name} has parameters :**\n ```{t_string}```")
-                    break
+            role = find_role(msg.guild, args[1])
+            if role:
+                t_dict = {
+                    "name": role.name,
+                    "permissions": role.permissions,
+                    "colour": role.colour,
+                    "hoist": role.hoist,
+                    "mentionable": role.mentionable,
+                    "position": role.position,
+                    "created_at": role.created_at,
+                    "id": role.id
+                }
+                t_string = ""
+                for k, v in t_dict.items():
+                    if k != "permissions":
+                        t_string = f"{t_string}{k}: {v!s}\n"
+                    else:
+                        t_string += k + ": " + ", ".join({x.upper() for x, y in v if y}) + "\n"
+                await respond(msg, f"**ANALYSIS: role {role.name} has parameters :**\n ```{t_string}```")
             else:
                 await respond(msg, f"**NEGATIVE. ANALYSIS: no role {name} found.**")
         else:
@@ -222,8 +218,5 @@ class RoleCommands(BasePlugin):
         """
         lists all roles along with position and color
         """
-        t_string = "**AFFIRMATIVE. Listing roles :**\n"
-        for role in msg.guild.role_hierarchy:
-            t_string += f"`{role.name[:40].ljust(40)} [{role.position:02d} | {role.colour}]`\n"
-        for t in split_message(t_string, splitter="\n"):
-            await respond(msg, t)
+        t_list = [f"{r.name[:40].ljust(40)} [{r.position:02d} | {r.colour}]" for r in msg.guild.role_hierarchy]
+        await split_output(msg, "**AFFIRMATIVE. Listing roles :**", t_list)
