7346f517afc7f8e1d525dd769ec1f1357c2391c5
MekaTriK <gtg3013@gmail.com>
2018-07-03 16:36:40 +0300

    Added direct source code upload option to !newcc. Usage: !newcc -s/--source ccname. If no name is provided, attempts to make one from the filename.

--- plugins/custom_commands.py
+++ plugins/custom_commands.py
@@ -107,7 +107,7 @@ class CustomCommands(BasePlugin):
                  "Tag Documentation: https://github.com/medeor413/Red_Star/wiki/Custom-Commands",
              syntax="(name) (content)",
              category="custom_commands")
-    async def _createcc(self, msg):
+    async def _createcc(self, msg: discord.Message):
         gid = str(msg.guild.id)
         self._initialize(gid)
         if msg.author.id in self.storage[gid]["cc_create_ban"]:
@@ -115,13 +115,18 @@ class CustomCommands(BasePlugin):
         if msg.attachments:
             fp = BytesIO()
             await msg.attachments[0].save(fp)
-            self.logger.debug(fp.getvalue())
-            try:
-                jsdata = json.loads(fp.getvalue().decode())
-            except json.JSONDecodeError:
-                raise CommandSyntaxError("Uploaded file is not valid JSON!")
-            name = jsdata["name"].lower()
-            content = jsdata["content"]
+            # self.logger.debug(fp.getvalue())
+            args = msg.clean_content.split()[1:]
+            if args and args[0].lower() in ("-s", "--source"):
+                name = args[1].lower() if len(args) > 1 else msg.attachments[0].filename.rsplit('.', 1)[0]
+                content = fp.getvalue().decode()
+            else:
+                try:
+                    jsdata = json.loads(fp.getvalue().decode())
+                except json.JSONDecodeError:
+                    raise CommandSyntaxError("Uploaded file is not valid JSON!")
+                name = jsdata["name"].lower()
+                content = jsdata["content"]
         else:
             try:
                 args = msg.clean_content.split(None, 2)[1:]
