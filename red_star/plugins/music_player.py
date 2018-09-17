50c94dbd2c46064d75337aa5f25dbd2a1af38ad7
medeor413 <me@medeor.me>
2018-09-17 12:43:02 -0400

    Add async queue loading When a playlist is put into the bot, the bot will no longer wait for the entire playlist to be downloaded before beginning playback, making things much faster Fixed queue silently discarding elements when exceeding the maximum size Fixed output of longer queues with Queue command Made SongVolume actually do something Made per-server config actually work

--- red_star/plugins/music_player.py
+++ red_star/plugins/music_player.py
@@ -3,6 +3,7 @@ import logging
 from asyncio import get_event_loop
 from collections import deque
 from discord import FFmpegPCMAudio, PCMVolumeTransformer
+from discord.errors import ClientException
 from math import floor
 from functools import partial
 from time import monotonic as time
@@ -10,7 +11,7 @@ from youtube_dl import YoutubeDL
 from red_star.command_dispatcher import Command
 from red_star.plugin_manager import BasePlugin
 from red_star.rs_errors import UserPermissionError, CommandSyntaxError
-from red_star.rs_utils import respond
+from red_star.rs_utils import respond, split_output
 
 class MusicPlayer(BasePlugin):
     name = "music_player"
@@ -99,14 +100,11 @@ class MusicPlayer(BasePlugin):
         elif len(player.queue) < 1 and not player.current_song:
             await respond(msg, "**ANALYSIS: The queue is empty.**")
             return
-        final = ""
         if player.current_song:
             now_playing = self.now_playing(player)
-            final += f"**ANALYSIS: Now playing:**\n```{now_playing}```"
+            await respond(msg, f"**ANALYSIS: Now playing:**\n```{now_playing}```")
         if player.queue:
-            queue_list = self.print_queue(player)
-            final += f"**ANALYSIS: Current queue:**\n```{queue_list}```"
-        await respond(msg, final)
+            await split_output(msg, "**ANALYSIS: Current queue:**\n", self.print_queue(player))
 
     @Command("SongVolume", "Volume",
              doc="Sets the volume at which the player plays music, between 0 and 100.",
@@ -115,14 +113,17 @@ class MusicPlayer(BasePlugin):
         player = self.get_guild_player(msg)
         self.check_user_permission(msg.author, player)
         try:
-            new_volume = msg.clean_content(None, 1)[1]
+            new_volume = msg.clean_content.split(None, 1)[1]
             new_volume = int(new_volume)
             if new_volume == 0:
-                raise CommandSyntaxError("Please use PauseSong to mute the bot.")
-            elif 0 < new_volume <= 100:
-                raise CommandSyntaxError("Expected value between 0 and 100.")
+                raise CommandSyntaxError("Please use PauseSong to mute the bot")
+            elif not 0 < new_volume <= 100:
+                raise CommandSyntaxError("Expected value between 0 and 100")
         except ValueError:
-            raise CommandSyntaxError(f"Value {new_volume} is not a valid integer.")
+            raise CommandSyntaxError(f"Value {new_volume} is not a valid integer")
+        except IndexError:
+            await respond(msg, f"**ANALYSIS: Current volume is {int(player.volume * 100)}%.**")
+            return
         player.volume = new_volume / 100
         await respond(msg, f"**AFFIRMATIVE. Set volume to {new_volume}%.**")
 
@@ -189,6 +190,8 @@ class MusicPlayer(BasePlugin):
             return self.config["per_server_configs"][gid][key]
         except KeyError:
             self.config["per_server_configs"][gid] = self.config["per_server_configs"]["default"].copy()
+            self.storage.save()
+            return self.config["per_server_configs"][gid][key]
 
     @staticmethod
     def print_queue(player):
@@ -211,7 +214,7 @@ class MusicPlayer(BasePlugin):
             if len(title) >= 59:
                 title = title[:56] + "..."
             str_list.append(f"[{i+1:02d}][{title:-<59}][{duration}]")
-        return "\n".join(str_list)
+        return str_list
 
     @staticmethod
     def now_playing(player):
@@ -240,28 +243,62 @@ class GuildPlayer:
         self.text_channel = channel
         self.voice_client = voice_client
         self.logger = logging.getLogger(f"red_star.plugin.music_player.player_{self.voice_client.guild.id}")
-        self.queue = deque(maxlen=self.parent.get_guild_config(self.voice_client.guild, "max_queue_length"))
+        self.queue = deque()
         self.is_playing = False
         self.current_song = {}
-        self.volume = self.parent.config["default_volume"] / 100
+        self._volume = self.parent.config["default_volume"] / 100
         self.loop = get_event_loop()
         self.song_start_time = None
         self.song_pause_time = None
 
     async def enqueue(self, url):
-        count = 1
         with self.text_channel.typing():
             with YoutubeDL(self.parent.ydl_options) as ydl:
                 vid_info = await self.loop.run_in_executor(None, partial(ydl.extract_info, url, download=False))
             if vid_info.get("_type") == "playlist":
-                for inf in vid_info["entries"]:
-                    self.queue.append(inf)
-                count = len(vid_info["entries"])
+                self.loop.create_task(self.enqueue_playlist(vid_info["entries"]))
+                return
             else:
-                self.queue.append(vid_info)
-        await self.text_channel.send(f"**ANALYSIS: Queued {count} songs for listening.**")
+                if len(self.queue) < self.parent.get_guild_config(self.voice_client.guild, "max_queue_length"):
+                    await self.process_video(vid_info)
+                else:
+                    await self.text_channel.send(f"**WARNING: The queue is full. {vid_info['title']} "
+                                                 f"will not be added.")
+        await self.text_channel.send(f"**ANALYSIS: Queued `{vid_info['title']}`.**")
         if not self.is_playing:
             await self.play()
+
+    async def enqueue_playlist(self, entries):
+        await self.text_channel.send(f"**ANALYSIS: Attempting to queue {len(entries)} videos. Your playback will "
+                                     f"begin shortly.")
+        orig_len = len(self.queue)
+        with self.text_channel.typing():
+            for vid in entries:
+                if len(self.queue) >= self.parent.get_guild_config(self.voice_client.guild, "max_queue_length"):
+                    await self.text_channel.send(f"**WARNING: The queue is full. No more videos will be added.**")
+                    break
+                try:
+                    await self.process_video(vid)
+                except TypeError:
+                    continue
+                self.logger.info(f"Added {vid['title']} to queue.")
+                if not self.is_playing:
+                    try:
+                        self.loop.create_task(self.play())
+                    except ClientException:
+                        pass
+        await self.text_channel.send(f"**ANALYSIS: Queued {len(self.queue) - orig_len} videos.**")
+
+    async def process_video(self, vid):
+        if not vid:
+            raise TypeError
+        if self.parent.config["save_audio"] and not vid.get("is_live", False):
+            with YoutubeDL(self.parent.ydl_options) as ydl:
+                await self.loop.run_in_executor(None, partial(ydl.process_info, vid))
+                vid["filename"] = ydl.prepare_filename(vid)
+        vid.setdefault("title", "Unknown")
+        vid.setdefault("is_live", False)
+        self.queue.append(vid)
 
     async def play(self):
         try:
@@ -277,20 +314,18 @@ class GuildPlayer:
                 pass
             return
         before_args = ""
-        if self.parent.config["save_audio"] and not next_song.get("is_live", False):
-            with YoutubeDL(self.parent.ydl_options) as ydl:
-                await self.loop.run_in_executor(None, partial(ydl.process_info, next_song))
-                file = ydl.prepare_filename(next_song)
+        if self.parent.config["save_audio"] and not next_song["is_live"]:
+            file = next_song["filename"]
         else:
             file = next_song["url"]
             before_args += " -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 30"
-        source = PCMVolumeTransformer(FFmpegPCMAudio(file, before_options=before_args, options="-vn"),
-                                      volume=self.volume)
-        self.voice_client.play(source, after=self.after)
         self.is_playing = True
+        source = PCMVolumeTransformer(FFmpegPCMAudio(file, before_options=before_args, options="-vn"),
+                                      volume=self._volume)
+        self.voice_client.play(source, after=self.after)
         self.song_start_time = time()
         self.current_song = next_song
-        await self.text_channel.send(f"**NOW PLAYING: {next_song.get('title', 'Unkown')}.**")
+        await self.text_channel.send(f"**NOW PLAYING: {next_song['title']}.**")
 
     def toggle_pause(self):
         if self.voice_client.is_paused():
@@ -321,3 +356,12 @@ class GuildPlayer:
             return self.song_pause_time - self.song_start_time
         else:
             return time() - self.song_start_time
+
+    @property
+    def volume(self):
+        return self.volume
+
+    @volume.setter
+    def volume(self, val):
+        self._volume = val
+        self.voice_client.source.volume = val
