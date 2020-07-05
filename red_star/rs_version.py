b6d13da44b2f1e401b5f6136fd5690acb0b570d5
medeor413 <me@medeor.me>
2020-07-05 12:14:41 -0400

    Finalize version 2.1.6

--- red_star/rs_version.py
+++ red_star/rs_version.py
@@ -10,6 +10,6 @@ class VersionInfo(namedtuple("VersionInfo", 'major minor patch releaselevel')):
         return verstr
 
 
-version_tuple = VersionInfo(major=2, minor=1, patch=6, releaselevel="beta")
+version_tuple = VersionInfo(major=2, minor=1, patch=6, releaselevel="release")
 
 version = version_tuple.as_string()
