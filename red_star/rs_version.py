708903b80d35b4db1cc44f38aff3c3a1a4b45a22
Robin <me@medeor.me>
2022-05-06 16:29:48 -0400

    Bump version number

--- red_star/rs_version.py
+++ red_star/rs_version.py
@@ -10,6 +10,6 @@ class VersionInfo(namedtuple("VersionInfo", 'major minor patch releaselevel')):
         return verstr
 
 
-version_tuple = VersionInfo(major=2, minor=2, patch=4, releaselevel="release")
+version_tuple = VersionInfo(major=2, minor=3, patch=0, releaselevel="alpha")
 
 version = version_tuple.as_string()
